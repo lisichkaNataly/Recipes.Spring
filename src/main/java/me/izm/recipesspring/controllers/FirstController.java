61659347d43b7b159b4aa5a15d993d34831685a2
Izmaylova <Natalyatf@yandex.ru>
2022-12-12 01:27:56 +0500

    Домашнее задание

--- src/main/java/me/izm/recipesspring/controllers/FirstController.java
+++ src/main/java/me/izm/recipesspring/controllers/FirstController.java
@@ -1,7 +1,20 @@
 package me.izm.recipesspring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(@RequestParam String name, String nameProject, String date, String description) {
+        return "Информация о проекте: Имя ученика: " + name
+                + " название проекта: " + nameProject + " дата создания: " + date + " описание проекта: " + description;
+    }
 }
